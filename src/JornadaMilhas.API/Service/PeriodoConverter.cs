2e9bc2a6d79ccac8f5f35b670e8a0e86c142fe13
André Santana <andrelms4@insper.edu.br>
2024-03-11 09:12:36 -0300

    Adicionar arquivos de projeto.

---
+++ src/JornadaMilhas.API/Service/PeriodoConverter.cs
@@ -0,0 +1,21 @@
+﻿using JornadaMilhas.API.DTO.Request;
+using JornadaMilhas.API.DTO.Response;
+using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+
+namespace JornadaMilhas.API.Service;
+
+public class PeriodoConverter
+{
+    public Periodo RequestToEntity(PeriodoRequest periodoRequest)
+    {
+        return new Periodo(periodoRequest.dataInicial, periodoRequest.dataFinal) ;
+    }
+
+    public PeriodoResponse EntityToResponse(Periodo periodo)
+    {
+        return new PeriodoResponse(periodo.DataInicial,periodo.DataFinal);
+    }
+
+    
+}
